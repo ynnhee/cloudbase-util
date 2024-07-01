552bdfba0c38033c4d64c4e224c479bef488044f
lgc <570277935@qq.com>
2024-07-01 21:54:40 +0800

    feat: add get func

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const bytesToSize = require('./libs/byte_to_size.js');
 const convertTime = require('./libs/convert_time.js');
 const _ = require('lodash');
 const moment = require('moment-timezone');
+const env = {
+  image: process.env.YNNHEE_FUNC_CDN_IMAGE,
+  video: process.env.YNNHEE_FUNC_CDN_VIDEO,
+  audio: process.env.YNNHEE_FUNC_CDN_AUDIO,
+  image_prefix: process.env.YNNHEE_FUNC_CDN_IMAGE_PREFIX,
+  video_prefix: process.env.YNNHEE_FUNC_CDN_IMAGE_PREFIX,
+  audio_prefix: process.env.YNNHEE_FUNC_CDN_IMAGE_PREFIX,
+}
 
 module.exports = {
   _: _,
@@ -12,12 +20,14 @@ module.exports = {
   format: format,
   bytesToSize: bytesToSize,
   convertTime: convertTime,
-  env: {
-    image: process.env.YNNHEE_FUNC_CDN_IMAGE,
-    video: process.env.YNNHEE_FUNC_CDN_VIDEO,
-    audio: process.env.YNNHEE_FUNC_CDN_AUDIO,
-    image_prefix: process.env.YNNHEE_FUNC_CDN_IMAGE_PREFIX,
-    video_prefix: process.env.YNNHEE_FUNC_CDN_IMAGE_PREFIX,
-    audio_prefix: process.env.YNNHEE_FUNC_CDN_IMAGE_PREFIX,
-  }
+  getImageUrl: function (fid) {
+    return format(env.image, { fid: fid })
+  },
+  getVideoUrl: function (fid) {
+    return format(env.video, { fid: fid })
+  },
+  getAudioUrl: function (fid) {
+    return format(env.audio, { fid: fid })
+  },
+  env: env
 };
