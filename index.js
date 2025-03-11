60482b85f2359ad8e7fc7c576f7315248fd90b43
lgc <570277935@qq.com>
2025-03-11 15:48:53 +0800

    feat: add getDocsUrl function

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const env = {
   video: process.env.YNNHEE_FUNC_CDN_VIDEO,
   audio: process.env.YNNHEE_FUNC_CDN_AUDIO,
   avatar: process.env.YNNHEE_FUNC_CDN_AVATAR,
+  docs: process.env.YNNHEE_FUNC_CDN_DOCS,
   image_prefix: process.env.YNNHEE_FUNC_CDN_IMAGE_PREFIX,
   avatar_prefix: process.env.YNNHEE_FUNC_CDN_AVATAR_PREFIX,
   video_prefix: process.env.YNNHEE_FUNC_CDN_VIDEO_PREFIX,
   audio_prefix: process.env.YNNHEE_FUNC_CDN_AUDIO_PREFIX,
+  docs_prefix: process.env.YNNHEE_FUNC_CDN_DOCS_PREFIX,
 }
 
 module.exports = {
@@ -31,6 +33,9 @@ module.exports = {
   },
   getAudioUrl: function (fid) {
     return format(env.audio, { fid: fid })
+  },
+  getDocsUrl: function (fid) {
+    return format(env.docs, { fid: fid })
   },
   resetAvatarUrl: function (user_info) {
     if (user_info && _.isString(user_info.avatarUrl) && user_info.avatarUrl.includes('/miniprogram/') && user_info.avatarUrl.includes('/avatar/')) {
